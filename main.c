5b145d530b55607f6bb41d18ab35b01d7fca638a
Aapeli <git@aapelivuorinen.com>
2018-12-09 22:04:13 +1000

    Use fixed size types

--- main.c
+++ main.c
@@ -2,10 +2,10 @@
 
 #include "g711.h"
 
-void decode(long length, char *in_buf, short *out_buf) {
-    short temp_buf[length];
-    for (int i = 0; i < length; i++) {
-        temp_buf[i] = ((short) in_buf[i]) & 0x00FF;
+void decode(size_t length, int8_t *in_buf, int16_t *out_buf) {
+    int16_t temp_buf[length];
+    for (size_t i = 0; i < length; i++) {
+        temp_buf[i] = ((int16_t) in_buf[i]) & 0x00FF;
     }
     ulaw_expand(length, temp_buf, out_buf);
 }
@@ -21,13 +21,13 @@ int main(int argc, char **argv) {
     FILE *fp = fopen(argv[1], "r");
     FILE *fo = fopen(argv[2], "w+");
 
-    char  c_buffer[BUFFER_SIZE];
-    short o_buffer[BUFFER_SIZE];
+    int8_t  c_buffer[BUFFER_SIZE];
+    int16_t o_buffer[BUFFER_SIZE];
 
     int bytes_read = 0;
-    while ((bytes_read = fread(c_buffer, sizeof(char), BUFFER_SIZE, fp)) != 0) {
+    while ((bytes_read = fread(c_buffer, sizeof(int8_t), BUFFER_SIZE, fp)) != 0) {
         decode(bytes_read, c_buffer, o_buffer);
-        fwrite(o_buffer, sizeof(short), bytes_read, fo);
+        fwrite(o_buffer, sizeof(int16_t), bytes_read, fo);
     }
     return 0;
 }
