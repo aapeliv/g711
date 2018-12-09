63607dfa65b1e3aa46c223d42e93831c89dce192
Janis Lesinskis <janis@lesinskis.com>
2018-12-09 18:39:36 +1100

    Move python bindings into own directory

--- setup.py
+++ setup.py
@@ -2,6 +2,6 @@ from setuptools import setup
 setup(
     name = "g711",
     setup_requires = ["cffi>=1"],
-    cffi_modules=["python_g711.py:ffibuilder"],
+    cffi_modules=["python_bindings/python_g711.py:ffibuilder"],
     install_requires = ["cffi>=1"],
 )
